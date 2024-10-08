fd2583136741730c0482465e84367311f693b61a
Max <maxigiaco94@hotmail.com>
2024-10-08 10:33:45 -0300

    Change file structucture, add js folder in src, redirect imports

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   mode: "development",
-  entry: "./src/index.js", //ENTRY POINT
+  entry: "./src/js/index.js", //ENTRY POINT
   output: {
     filename: "main.js",
     path: path.resolve(__dirname, "dist"),
