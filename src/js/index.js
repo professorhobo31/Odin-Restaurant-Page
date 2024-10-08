b47cdc02c4b4bbe112eb0ac6fff77b766b43d6d1
Max <maxigiaco94@hotmail.com>
2024-10-08 18:30:38 -0300

    Add file loader, awesomefont and put temp icons in the navbar

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,7 @@
-import "../scss/header.scss";
+import "../scss/index.scss";
+import "@fortawesome/fontawesome-free/js/fontawesome";
+import "@fortawesome/fontawesome-free/js/solid";
+import "@fortawesome/fontawesome-free/js/regular";
+import "@fortawesome/fontawesome-free/js/brands";
 
 // alert("JS working dawg");
